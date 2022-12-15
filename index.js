bef0372db17305c0269bdf5f44af0fac1bf79d5d
taran <taranr5080@gmail.com>
2022-12-15 14:02:11 -0500

    Countinue working

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const ORGANISM_LIST = ["Air;None;None", "Earth;None;None", "Fire;None;None", "Water;None;None", "Alcohol;Fire;Water", "Dust;Air;Earth", "Energy;Air;Fire", "Lava;Earth;Fire", "Swamp;Earth;Water", "Mud;Dust;Water", "Life;Energy;Swamp", "Bacteria;Life;Swamp", "Fire Elemental;Fire;Life", "Stone;Air;Lava", "Metal;Stone;Fire", "Electricity;Energy;Metal", "Oxygen;Water;Electricity"];
+const ORGANISM_OFFSET = 4;
+const NAME = 0, PARENT_1 = 1, PARENT_2 = 2;
+const NONE = "None";
 const INVALID_CHARS = "<>@#*";
 
 function init() {
@@ -16,6 +20,17 @@ function init() {
     }
 
     displayList();
+
+    outBox = document.getElementById("outbox");
+    foodSelectionBox = document.getElementById("foodsell");
+    duplicatesBox = document.getElementById("dupes");
+    countBox = document.getElementById("count");
+
+    foodList = ["Pizza", "Hamburger", "Sushi", "Guacamole", "Salmon", "Hamburger", "Guacamole", "Hamburger", "Salmon", "Sushi", "Guacamole", "Guacamole", "Hamburger", "Sushi"];
+    dupesList = [];
+    dupesCount = 0;
+
+    display();
 }
 
 function addMessage(msg) {
@@ -55,6 +70,64 @@ function displayList() {
     arrayOutput.innerHTML = "";
 
     for (var i = 0; i < randomList.length; i++) {
-        arrayOutput.innerHTML += i + ": " randomList[i] + "<br />";
+        arrayOutput.innerHTML += i + ": " + randomList[i] + "<br />";
     }
+}
+
+function display() {
+    outBox.innerHTML = "0: " + foodList[0];
+    for (var i = 1; i < foodList.length; i++) {
+        outBox.innerHTML += "<br />" + i + ": " + foodList[i];
+    }
+    duplicatesBox.innerHTML = dupesList;
+    countBox.innerHTML = dupesCount;
+}
+
+function addFood() {
+    foodList.push(foodSelectionBox.value);
+    display();
+}
+
+function countDuplicates(array, itm) {
+    var itmCount = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        if (itm == array[i]) itmCount++;
+    }
+
+    return itmCount;
+}
+
+function indexesOf(array, itm) {
+    var idxList = [];
+    for (var i = 0; i < array.length; i++) if (itm == array[i]) idxList.push(i);
+    return idxList;
+}
+
+function getDupeList() {
+    dupesList = indexesOf(foodList, foodSelectionBox.value);
+    display();
+}
+
+function getDupeCount() {
+    dupesCount = countDuplicates(foodList, foodSelectionBox.value);
+    display();
+}
+
+function getOrganismData(organism, idx) {
+    return organism.split(";")[idx];
+}
+
+function areParents(organism, parent1, parent2) {
+    var firstParent = getOrganismData(organism, PARENT_1);
+    var secondParent = getOrganismData(organism, PARENT_2);
+    var parent1 = getOrganismData(parent1, NAME);
+    var parent2 = getOrganismData(parent2, NAME);
+    if (firstParent == parent1 || secondParent == parent1) {
+        if (firstParent == parent2 || secondParent == parent1) {
+            return true;
+        }
+    }
+
+    return false;
 }
