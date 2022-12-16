fb2e7329e45d2c68f21529398d29cdbc4de91f8e
Taran Roth <taranr5080@gmail.com>
2022-12-15 20:33:15 -0500

    finished classwork

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// I learned some ways that arrays can be used to make programs more efficient and structured.
+
 const ORGANISM_LIST = ["Air;None;None", "Earth;None;None", "Fire;None;None", "Water;None;None", "Alcohol;Fire;Water", "Dust;Air;Earth", "Energy;Air;Fire", "Lava;Earth;Fire", "Swamp;Earth;Water", "Mud;Dust;Water", "Life;Energy;Swamp", "Bacteria;Life;Swamp", "Fire Elemental;Fire;Life", "Stone;Air;Lava", "Metal;Stone;Fire", "Electricity;Energy;Metal", "Oxygen;Water;Electricity"];
 const ORGANISM_OFFSET = 4;
 const NAME = 0, PARENT_1 = 1, PARENT_2 = 2;
@@ -30,6 +32,15 @@ function init() {
     dupesList = [];
     dupesCount = 0;
 
+    myOrganismsOutput = document.getElementById("orgs");
+    firstSelectionOutput = document.getElementById("1st");
+    secondSelectionOutput = document.getElementById("2nd");
+    organismForm = document.getElementById("orgsform");
+
+    myOrganisms = [ORGANISM_LIST[0], ORGANISM_LIST[1], ORGANISM_LIST[2], ORGANISM_LIST[3]];
+    firstSelection = NONE;
+    secondSelection = NONE;
+
     display();
 }
 
@@ -41,10 +52,6 @@ function addMessage(msg) {
 
     log += "<br />" + validate(msg);
     display();
-}
-
-function display() {
-    logOutputBox.innerHTML = log;
 }
 
 function isValid(msg) {
@@ -72,15 +79,6 @@ function displayList() {
     for (var i = 0; i < randomList.length; i++) {
         arrayOutput.innerHTML += i + ": " + randomList[i] + "<br />";
     }
-}
-
-function display() {
-    outBox.innerHTML = "0: " + foodList[0];
-    for (var i = 1; i < foodList.length; i++) {
-        outBox.innerHTML += "<br />" + i + ": " + foodList[i];
-    }
-    duplicatesBox.innerHTML = dupesList;
-    countBox.innerHTML = dupesCount;
 }
 
 function addFood() {
@@ -130,4 +128,73 @@ function areParents(organism, parent1, parent2) {
     }
 
     return false;
+}
+
+function findOrganism(orgname) {
+    for (var i in ORGANISM_LIST) {
+        if (getOrganismData(ORGANISM_LIST[i], NAME) == orgname) return ORGANISM_LIST[i];
+    }
+}
+
+function findCombo(org1, org2) {
+    for (var i in ORGANISM_LIST) {
+        if (areParents(ORGANISM_LIST[i], org1, org2)) return ORGANISM_LIST[i];
+    }
+    return NONE;
+}
+
+function display() {
+    outBox.innerHTML = "0: " + foodList[0];
+    for (var i = 1; i < foodList.length; i++) {
+        outBox.innerHTML += "<br />" + i + ": " + foodList[i];
+    }
+    duplicatesBox.innerHTML = dupesList;
+    countBox.innerHTML = dupesCount;
+    logOutputBox.innerHTML = log;
+
+    myOrganismsOutput.innerHTML = "1: " + getOrganismData(myOrganisms[0], NAME);
+
+    for (var i = 1; i < myOrganisms.length; i++) {
+        myOrganismsOutput.innerHTML += "<br />" + (i + 1) + ": " + getOrganismData(myOrganisms[i], NAME);
+        firstSelectionOutput.innerHTML = getOrganismData(firstSelection, NAME);
+        secondSelectionOutput.innerHTML = getOrganismData(secondSelection, NAME);
+
+    }
+}
+
+function selectOrganism() {
+    var idx = parseInt(organismForm.orgse1.value)-1;
+
+    if (idx < 0 || idx >= myOrganisms.length) {
+        return
+    }
+
+    if (firstSelection == NONE || (firstSelection != NONE && secondSelection != NONE)) {
+        firstSelection = myOrganisms[idx];
+        secondSelection = NONE;
+    } else {
+        secondSelection = myOrganisms[idx];
+        addNewOrganism();
+    }
+    organismForm.orgse1.value = "";
+    display();
+}
+
+function addNewOrganism() {
+    var newOrganism = findCombo(firstSelection, secondSelection);
+    if (newOrganism != NONE) {
+        if (myOrganisms.indexOf(newOrganism) < 0) {
+            myOrganisms.push(newOrganism);
+        }
+    }
+}
+
+function deleteOrganism() {
+    var idx = parseInt(organismForm.orgse1.value)-1;
+    if (idx < ORGANISM_OFFSET || idx > myOrganisms.length) {
+        return;
+    }
+    myOrganisms.splice(idx, 1);
+    organismForm.orgse1.value = "";
+    display();
 }
